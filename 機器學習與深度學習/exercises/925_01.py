cfed915ed235c2f65b9a01783ea568a03b018e66
Weber12321 <36941261+Weber12321@users.noreply.github.com>
2019-10-02 10:32:16 +0800

    1002 w4 a1

--- 機器學習與深度學習/exercises/925_01.py
+++ 機器學習與深度學習/exercises/925_01.py
@@ -8,7 +8,7 @@ Created on Wed Sep 25 16:55:40 2019
 import logging
 from gensim.models import word2vec
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s :  %(message)s', level=logging.INFO)
-sentences = word2vec.LineSentence("text8")
+logging.basicCt='%(asctime)s : %(levelname)s :  %(message)s', level=logging.INFO)
+sentences = word2vec.Lonfig(formaineSentence("text8")
 model = word2vec.Word2Vec(sentences, size = 300)
 model.save("word2vec.model")
---
+++ 物件導向程式設計/W4_assignments/w4_a1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  2 10:20:12 2019
+
+@author: Weber
+"""
+
+print("通關密語")
+
+
+
+while True:
+    code = int(input("請輸入密碼!!!"))
+    if code == 1234:
+        print('歡迎光臨!')
+        break
+    else:
+        print ('密碼錯誤!')
+        continue
