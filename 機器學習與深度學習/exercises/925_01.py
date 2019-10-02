80b50435d3874861d26acf3ecc37dc6116d4fb76
Weber12321 <36941261+Weber12321@users.noreply.github.com>
2019-10-02 10:15:20 +0800

    1002

---
+++ 機器學習與深度學習/exercises/925_01.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 25 16:55:40 2019
+
+@author: Weber
+"""
+
+import logging
+from gensim.models import word2vec
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s :  %(message)s', level=logging.INFO)
+sentences = word2vec.LineSentence("text8")
+model = word2vec.Word2Vec(sentences, size = 300)
+model.save("word2vec.model")
