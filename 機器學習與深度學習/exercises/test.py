0dd5878bc0fe812b5483b5fc83eeddfbe219ee6c
Weber12321 <36941261+Weber12321@users.noreply.github.com>
2019-10-07 21:38:37 +0800

    1007

--- 機器學習與深度學習/exercises/test.py
+++ 機器學習與深度學習/exercises/test.py
@@ -5,11 +5,7 @@ Created on Sat Oct  5 14:30:34 2019
 @author: Weber
 """
 
-import string
-import zhon.hanzi
-
-t = ['12d$%^＠＃＄％','231}{_','21|}','234','12']
-
-t = [''.join(c for c in s if c not in string.punctuation) for s in t]
-t = [''.join(c for c in s if c not in zhon.hanzi.punctuation) for s in t]
-print(t)
+x = list(range(1,3))
+y = {'1':1,'2':2,'3':3,'4':4}
+print(x)
+print(list(y.values()))
