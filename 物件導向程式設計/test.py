15b59cc7170e9c5da697580ff81f931e477ab70a
Weber12321 <36941261+Weber12321@users.noreply.github.com>
2019-12-18 14:44:32 +0800

    1218

--- 物件導向程式設計/test.py
+++ 物件導向程式設計/test.py
@@ -2,8 +2,40 @@ import unittest
 
 class TestStringMethods(unittest.TestCase):
 
+    def setUp(self):
+        print('setUp...')
+        self._text = 'foo'
+
+    def tearDown(self):
+        print('tearDown...')
+        self._text = ''
+
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
-
+    
     def test_isupper(self):
-        
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = '台北醫學大學'
+        result = ngram(s, 2)
+        self.assertEqual(result[0], '台北')
+        self.assertEqual(result[1], '北醫')
+        self.assertEqual(result[2], '醫學')
+        self.assertEqual(result[3], '學大')
+        self.assertEqual(result[4], '大學')
+
+        esult = ngram(s, 3)
+        self.assertEqual(result[0], '台北醫')
+        self.assertEqual(result[1], '北醫學')
+        self.assertEqual(result[2], '醫學大')
+        self.assertEqual(result[3], '學大學')
+
+    def ngram(s, n):
+        result = []
+        return result
+
+if __name__ == "__main__":
+    unittest.main()
+
